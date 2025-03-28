d72ebbfc978dd89ca4a7ec0f70f7712a3b4a9ef9
tryhard0000 <bhustle443@gmail.com>
2025-03-29 01:23:25 +0300

    s

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,10 @@
 
-import { getSession } from "@/app/api/auths/getSession"; 
 import LoginClient from "@/components/loginclient/page";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 
 
 const Login = async () => {
 
-   const session = await getSession();
-   const user = session?.user;
-   if(user) redirect("/")
     
   return (
     <div>
